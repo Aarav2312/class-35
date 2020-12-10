ad22526abe2efe4344de8582847e32ee4a934200
Aarav2312 <71076683+Aarav2312@users.noreply.github.com>
2020-12-10 16:56:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var player,form,game;
+var database;
+var gameState=0
+var playerCount
+
+function setup(){
+  createCanvas(400,400)
+  database = firebase.database();
+  game=new Game()
+    game.getState()
+    game.start()
+  
+  
+}
+
+function draw(){
+ 
+    drawSprites();
+  
+}
+
+
